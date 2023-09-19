91f2907504c765845d49604a2a3501238621b12f
Mikayel <gyogchyanmikayel@gmail.com>
2023-09-19 19:38:51 +0000

    Update app.js
    Handling POST Requests

--- app.js
+++ app.js
@@ -3,24 +3,8 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...')
-// })
-
-/*
-v1 is the API version
-It is a good practice to specify the API version, that in case you do 
-some changes in your API you do that in but simply ten on v2, withot 
-breaking everyone who is still using v1.
-We always have to specify the version of the API.
-We include it in the url.
-*/
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -30,14 +14,35 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'success',
     results: tours.length,
     data: {
-      tours: tours
-    }
-  })
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      }
+    })}
+  );
 });
 
 const port = 3000;
-// starts up a serveer // it's to what we did with the http package
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// status(200) ok
+// status(201) created
