e2bfcd5bb12822e2ec91283cba67a46064edd241
Mikayel <gyogchyanmikayel@gmail.com>
2023-09-29 15:28:13 +0000

    Add files via upload
    Param Middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require("./../controllers/tourController");
 
 const router = express.Router()
 
+router.param('id', tourController.checkID);
+
 router
   .route('/') // new one
   .get(tourController.getAllTours)
