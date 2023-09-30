f407ddf8ad6c41547ad97a5e3ad2f99c5e0a34d6
Mikayel <gyogchyanmikayel@gmail.com>
2023-09-30 18:58:24 +0000

    Add files via upload
    Environment Variables

--- app.js
+++ app.js
@@ -8,25 +8,32 @@ const app = express();
 
 // 1. Middlewares
 
-app.use(morgan('dev'))
+// only runs this middleware (to only define it) when we are in 'development',
+// so that the login does not happen when the app is in 'production'
+
+// console.log(process.env.NODE_ENV); 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the Middleware');
   next();
 });
- 
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 // 3. Routes
 
 // Mount our routers
-// These 2 routes are middleware, which is why we can use app.use() in order to mount them 
+// These 2 routes are middleware, which is why we can use app.use() in order to mount them
 app.use('/api/v1/tours', tourRouter); // for 'api/v1/tours' we want to apply 'tourRouter'
 app.use('/api/v1/users', userRouter);
 
- module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const app = require('./app')
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' }); // the reading of the variables from the file which happens here, only needs to happen once. It's then in the process and the process is the same, no matter in what file we are
 
-const port = 3000;
+const app = require('./app');
+
+
+// SET NODE_ENV=development nodemon server.js
+
+// console.log(app.get('env')); // development // thst is the environment that we are currently in.
+// console.log(process.env); // production
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
