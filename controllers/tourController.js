5048cdc6c67afbae2d162bbea3bf4aaad520e67b
Mikayel <gyogchyanmikayel@gmail.com>
2023-11-27 15:37:09 -0100

    Add files via upload
    Making the API Better: Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,22 +20,32 @@ exports.getAllTours = async (req, res) => {
     // 2. Sorting
     if(req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ')
-      console.log(sortBy); // -price ratingsAverage
+      console.log(sortBy); 
       query = query.sort(sortBy)
-      // sort('price, ratingAverage')
-
-      // Ascending '127.0.0.1:3000/api/v1/tours?sort=price' // 1, 2, 3
-      // Descending '?sort=-price' // 3, 2, 1
+      
     } else {
       query = query.sort('-createdAt')
     }
 
     // 3. Field Limiting
-    if(req.query.fields) { // 127.0.0.1:3000/api/v1/tours/?fields=name,duration,difficulty,price
+    if(req.query.fields) { 
       const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields) // the operation of selecting only certain fieldnames is called "projectiing"
+      query = query.select(fields) 
     } else {
-      query = query.select('-__v') // '-' is NOT including but excluding, we don't need to send it to the client
+      query = query.select('-__v') 
+    }
+
+    // 4. Pagination
+    const page = req.query.page * 1 || 1 // * 1 to convert the string to number. Each time that we put a number in a query string, that will be a string in our query object, so we fix that multiplying by 1
+                                         // || 1 (the 1 is the default value ) 
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+    // page=2&limits=10 
+    query = query.skip(skip).limit(limit) // skip(2) - the amount of results that should be skipped before querying data. limit(10) - the amount of results that we want in query 
+
+    if(req.query.page) {
+      const numTours = await Tour.countDocuments() // countDocuments() returns the number of the documents 
+      if(skip >= numTours) throw new Error('This page does not exist')
     }
 
     // EXECUTE QUERY
@@ -116,7 +126,7 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    await Tour.findByIdAndDelete(req.params.id); // in this case we don't save in a variable, because we don't send anything back to the client. In a RESTful API it is a common practice not to save any data to the client when there was a delete operation.
+    await Tour.findByIdAndDelete(req.params.id); 
     res.status(204).json({
       status: 'success',
       data: null
