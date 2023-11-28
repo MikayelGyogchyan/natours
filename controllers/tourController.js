f344c60327836eb73f92c664460ebfba21c513c5
Mikayel <gyogchyanmikayel@gmail.com>
2023-11-28 19:47:35 -0100

    Add files via upload
    Making the API Better: Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,51 +1,60 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  // Prefilling parts of the query object before we reach the getAllTours handler.
+  // And as soon as we get to the 'getAllTours' function, the query object is already prefilled  
+  // we prefilled the query string, so that the user doesn't have to do it on its own 
+  req.query.limit = '5'
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+  next()
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query); 
-    
+    console.log(req.query);
+
     // BUILD QUERY
     // 1A. Filtring
     const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields']; 
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
     // 1B. Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); 
-    console.log(JSON.parse(queryStr)); 
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2. Sorting
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      console.log(sortBy); 
-      query = query.sort(sortBy)
-      
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt')
+      query = query.sort('-createdAt');
     }
 
     // 3. Field Limiting
-    if(req.query.fields) { 
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields) 
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
     } else {
-      query = query.select('-__v') 
+      query = query.select('-__v');
     }
 
     // 4. Pagination
-    const page = req.query.page * 1 || 1 // * 1 to convert the string to number. Each time that we put a number in a query string, that will be a string in our query object, so we fix that multiplying by 1
-                                         // || 1 (the 1 is the default value ) 
-    const limit = req.query.limit * 1 || 100
-    const skip = (page - 1) * limit
-    // page=2&limits=10 
-    query = query.skip(skip).limit(limit) // skip(2) - the amount of results that should be skipped before querying data. limit(10) - the amount of results that we want in query 
+    const page = req.query.page * 1 || 1; // * 1 to convert the string to number. Each time that we put a number in a query string, that will be a string in our query object, so we fix that multiplying by 1
+    // || 1 (the 1 is the default value )
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limits=10
+    query = query.skip(skip).limit(limit); // skip(2) - the amount of results that should be skipped before querying data. limit(10) - the amount of results that we want in query
 
-    if(req.query.page) {
-      const numTours = await Tour.countDocuments() // countDocuments() returns the number of the documents 
-      if(skip >= numTours) throw new Error('This page does not exist')
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments(); // countDocuments() returns the number of the documents
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // EXECUTE QUERY
@@ -126,7 +135,7 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    await Tour.findByIdAndDelete(req.params.id); 
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
       data: null
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,10 +3,12 @@ const tourController = require("./../controllers/tourController");
 
 const router = express.Router()
 
-// router.param('id', tourController.checkID);
+router.route('/top-5-cheap') // 127.0.0.1:3000/api/v1/tours/top-5-cheap
+// we have to put the 'tours', because we are in the tours resource and the whole router is mounted onto this tours route, and its in the 'tours' that we created the 'top-5-cheap'
+  .get(tourController.aliasTopTours, tourController.getAllTours)
 
 router
-  .route('/') // new one
+  .route('/') 
   .get(tourController.getAllTours)
   .post(tourController.createTour);
 
