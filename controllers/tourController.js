8735947be8bde02fb969ee467e08e9f8f7e0f7c5
Mikayel <gyogchyanmikayel@gmail.com>
2023-11-29 19:38:24 -0100

    Add files via upload
    Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,64 +1,28 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   // Prefilling parts of the query object before we reach the getAllTours handler.
-  // And as soon as we get to the 'getAllTours' function, the query object is already prefilled  
-  // we prefilled the query string, so that the user doesn't have to do it on its own 
-  req.query.limit = '5'
-  req.query.sort = '-ratingsAverage,price'
-  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
-  next()
+  // And as soon as we get to the 'getAllTours' function, the query object is already prefilled
+  // we prefilled the query string, so that the user doesn't have to do it on its own
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
 };
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-
-    // BUILD QUERY
-    // 1A. Filtring
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1B. Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2. Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3. Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4. Pagination
-    const page = req.query.page * 1 || 1; // * 1 to convert the string to number. Each time that we put a number in a query string, that will be a string in our query object, so we fix that multiplying by 1
-    // || 1 (the 1 is the default value )
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // page=2&limits=10
-    query = query.skip(skip).limit(limit); // skip(2) - the amount of results that should be skipped before querying data. limit(10) - the amount of results that we want in query
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments(); // countDocuments() returns the number of the documents
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+    // all of these chaining here works because after calling each of these methods we always return 'this', and 'this' is the object itself which has access to each of these methods 
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    // we keep adding stuff to the query until the end 
+    // then by the end, by the end, we await the result of that query, so that it can come back with all the documents that were selected. That query now lives in 'features', which is this object
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
