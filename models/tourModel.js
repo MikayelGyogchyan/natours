486b13ce36f6b530a6bde63173cd0c35472c3726
Mikayel <gyogchyanmikayel@gmail.com>
2023-12-04 20:35:13 -0100

    Add files via upload
    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,7 +55,11 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(), 
     select: false 
   },
-  startDates: [Date]
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false // usually tours are not secret
+  }
 }, 
 
 {
@@ -67,35 +71,36 @@ tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7 
 })
 
-// 105. Document Middleware
-
-// DOCUMENT MIDDLEWARE: runs before .save() and .create(). Only in .save() and .create() this middleware is gonna be executed, and not with find() or update()...
-// this is for pre middleware which is gonna run before an actual event. And that event is the 'save' event
-tourSchema.pre('save', function(next) { // this function will be called before an actual document is saved to the db 
-  // console.log(this);
+tourSchema.pre('save', function(next) { 
   this.slug = slugify(this.name, {lower: true})
-  next() // next() will call the next middleware in the stack
+  next() 
 }) 
 
-// tourSchema.pre('save', function(next) {
-//   console.log('Will save doc...');
-//   next()
-// })
+// 106. Query Middleware
+/*
+Query Middleware allows us to run functions before or after a certain query
+is executed.
+*/
 
-// // post middleware functions are executed after all the pre middleware functions have completed 
-// tourSchema.post('save', function(doc, next) { // post middleware has access not only to 'next()' but also to the document that was just saved to the database
-//   console.log(doc); // logging the finished doc
-//   next()
-// })
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) { // /^find/ is regExp, which says that this middleware should be executed for all the commands (strings) that start with the name find 
+  // find will make this query middleware and not doc middleware
+  // here the 'this' will now point at the current query and not at the current document. Because we are proccessing a query here
+  // creating secret tour field
+  this.find({secretTour: { $ne: true }})
+
+  this.start = Date.now() // line 98
+  next()
+})
+// Specifying a post middleware for find
+// this middleware will run after the query has already executed. Therefore it can have access to the docs that were returned
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`); // Query took 132 milliseconds
+  console.log(docs);
+  next()
+})
 
 const Tour = new mongoose.model('Tour', tourSchema); 
 
 module.exports = Tour
 
-/*
-We can have middleware running before and after a certain event. 
-And in the case of document middleware that event is usually the 'save' event.
-Then in the middle of a function itself, we have access to the 
-'this' keyword which is going to point at the currently being saved 
-document
-*/
