85b52942aa0c9d18bb9d5784c7043d6067cc80d4
Mikayel <gyogchyanmikayel@gmail.com>
2023-12-08 19:21:41 -0100

    Add files via upload
    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const app = express();
 
 // 1. Middlewares
 
-// only runs this middleware (to only define it) when we are in 'development',
-// so that the login does not happen when the app is in 'production'
-
-// console.log(process.env.NODE_ENV); 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -20,20 +16,20 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // 3. Routes
 
-// Mount our routers
-// These 2 routes are middleware, which is why we can use app.use() in order to mount them
-app.use('/api/v1/tours', tourRouter); // for 'api/v1/tours' we want to apply 'tourRouter'
+app.use('/api/v1/tours', tourRouter); 
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  })
+})
 
 module.exports = app;
