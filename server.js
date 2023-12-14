f70ee8a6d0769945d56b8c3dfb67b6136e323f1c
Mikayel <gyogchyanmikayel@gmail.com>
2023-12-14 14:05:39 -0100

    Add files via upload
    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' }); 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -19,7 +19,14 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
- 
+
+process.on('unhandledrejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shouting down...');
+  server.close(() => {
+    process.exit(1)
+  })
+});
