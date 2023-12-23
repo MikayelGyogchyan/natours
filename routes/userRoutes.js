4e2cb1ba45244f2d9130ddcd9416cebca59d7e88
Mikayel <gyogchyanmikayel@gmail.com>
2023-12-23 16:39:51 -0100

    Add files via upload
    Updating the Current User: Password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,20 +2,27 @@ const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
-router.post('/forgotPassword', authController.forgotPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
 
 router
   .route('/')
   .get(userController.getAllUsers)
-  .post(userController.createUser)
+  .post(userController.createUser);
 
-router.route('/:id')  
+router
+  .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
